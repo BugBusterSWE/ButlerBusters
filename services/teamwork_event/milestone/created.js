42c1ce4dc3f8e04784083695bbffc06a16a9aa86
korut94 <andreamnt94@gmail.com>
2016-03-14 22:53:15 +0100

    Remove all log print

--- services/teamwork_event/milestone/created.js
+++ services/teamwork_event/milestone/created.js
@@ -2,8 +2,6 @@ const fs = require( "fs" );
 const request = require( "request" );
 
 exports.main = function ( req, teamwork, github ) {
-    console.log( "==================================================" );
-    console.log( "Milestone created event occurred" );
     var idMilestone = req.body.objectId;
 
     var configRepo = JSON.parse( fs.readFileSync( "config/repo_info.json", "utf-8" ) );
@@ -21,7 +19,6 @@ exports.main = function ( req, teamwork, github ) {
     teamwork.retrieveMilestone( idMilestone, function ( err, response, body ) {
 	var info = JSON.parse( body );
 
-	console.log( "Milestone/created: building milestone..." );
 	// Push the new incomplete milestones to create new one when an repo tag will attach to the milestone.
 	// Use idMilestone to create a map key value simplifing the extraction.
 	configRepo["pending_milestones"][idMilestone] = {
@@ -32,10 +29,6 @@ exports.main = function ( req, teamwork, github ) {
 	    // Subsitute the data compact format in ISO 8601 format. The hour is not included.
 	    due_on: info["milestone"]["deadline"].replace( /(\d{4})(\d{2})(\d{2})/, "$1-$2-$3T00:00:00Z" )
 	};
-	console.log( "Milestone/created: done" );
-	console.log( "Milestone/created: configuration milestone" );
-	console.log( configRepo["pending_milestones"][idMilestone] );
-	console.log( "Milestone/created: save milestone..." );
 	// Save pending milestone
 	fs.writeFileSync( "config/repo_info.json", JSON.stringify( configRepo ), "utf-8" );
 	// Send positive result
@@ -54,7 +47,5 @@ exports.main = function ( req, teamwork, github ) {
 		    console.log( "Error send messages" );
 		}
 	});
-	console.log( "Milestone/created: done" );
-	console.log( "==================================================" );
     });	
 };
